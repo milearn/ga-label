cb5605df83dec55b0c794c5d85478fc716baef07
Milan Gupta <milangupta511@gmail.com>
2022-06-08 01:27:35 +0530

    fix: testing

--- label.js
+++ label.js
@@ -1,6 +1,17 @@
 const core = require("@actions/core");
 const { getOctokit, context } = require("@actions/github");
-
+function getLabelObj(str) {
+    const obj = {}
+    const lines = str.split('\n');
+    if(lines.length === 0) {
+        core.setFailed('labels syntax is wrong or not provided');
+    }
+    lines.forEach(function(line) {
+        const [baseBranch, labels] = line.split(":").map(val => val.trim());
+        obj[baseBranch] = labels.split(",").map(val => val.trim());
+    })
+    return obj
+}
 (async () => {
   try {
       
@@ -12,16 +23,22 @@ const { getOctokit, context } = require("@actions/github");
     }
     const octokit = getOctokit(githubToken);
     const { owner, repo } = context.repo;
-    const labels = core.getInput('labels');
-    console.log(context.payload.pull_request.base.ref);
-    console.log(labels);
-    
-    const defaultLabels = core
-      .getInput("default")
-      .split("\n")
-      .filter((x) => x !== "");
-    const issueNumber = context.issue.number;
+    const labelsObj = getLabelObj(core.getInput('labels'));
+    const baseBranch = context.payload.pull_request.base.ref;
+    let labels = labelsObj[baseBranch];
 
+    if(!labels || labels.length === 0) {
+        labels = core
+        .getInput("default")
+        .split("\n")
+        .filter((x) => x !== "");
+        
+    }
+    if(labels.length === 0) {
+        core.setOutput(`No label found for ${baseBranch}. Skipping....`)
+        return;
+    }
+    const issueNumber = context.issue.number;
     core.info(`Add labels: ${labels} to ${owner}/${repo}#${issueNumber}`);
 
     await octokit.rest.issues.addLabels({
